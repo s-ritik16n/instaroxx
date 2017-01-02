979b5b930fc2f918bc7ba15cb188491e7b630ab5
Ritik <saxenar444@gmail.com>
2017-01-02 10:48:47 +0530

    remove json stringify

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ app.get('/',function(req,res){
 
 app.get('/home',function(req,res){
   req.session.code = req.query.code;
-  var data = JSON.stringify({
+  var data = {
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
       grant_type: "authorizaton_code",
       redirect_url: "https://igroxx.herokuapp.com/home",
       code: "code"
-  })
+  }
   var options = {
       host: 'api.instagram.com',
       path:'/oauth/access_token',
