56712d0bfe9e4e2c713a87ab71a8bdf22fa71644
ritik16n <saxenar444@gmail.com>
2017-06-24 12:20:58 +0530

    data cleaned, pending nextpage challenge

--- python/src.py
+++ python/src.py
@@ -1,20 +1,55 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import sys
+import ast
+from io import StringIO
 
-#s = requests.Session()
-#res = s.get("https://www.instagram.com")
+class Scrapper(object):
+    """docstring for Scrapper."""
+    def __init__(self, username,password):
+        super(Scrapper, self).__init__()
+        self.session = requests.Session()
+        self.session.headers.update({'Referer':'https://www.instagram.com/'})
+        self.req = self.session.get('https://www.instagram.com/')
+        self.session.headers.update({'X-CSRFToken':self.req.cookies['csrftoken']})
+        self.username = username
+        self.password = password
+        self.login()
 
-session = requests.Session()
-session.headers.update({'Referer':'https://www.instagram.com/'})
-req = session.get('https://www.instagram.com/')
-session.headers.update({'X-CSRFToken':req.cookies['csrftoken']})
-login_data = {'username': 'ritik16n', 'password': ''}
-login = session.post('https://www.instagram.com/accounts/login/ajax/', data=login_data, allow_redirects=True)
-session.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
-cookies = login.cookies
-print(login.text)
-resp = session.get("https://www.instagram.com/" + "ritik16n")
-#print(resp.text)
-shared_data = resp.text.split("window._sharedData = ")[1].split(";</script>")[0]
-print(json.loads(shared_data)['entry_data']['ProfilePage'][0]['user'])
+    def login(self):
+        login_data = {'username': self.username, 'password': self.password}
+        login = self.session.post('https://www.instagram.com/accounts/login/ajax/', data=login_data, allow_redirects=True)
+        self.session.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
+        self.cookies = login.cookies
+        self.resp = self.session.get("https://www.instagram.com/" + self.username)
+        data = self.login_data()
+        self.clean_up(data)
+
+    def login_data(self):
+        shared_data = self.resp.text.split("window._sharedData = ")[1].split(";</script>")[0]
+        json_data = shared_data
+        return json_data
+
+    def clean_up(self,data):
+        self.extracted_data = unicode(ast.literal_eval(json.dumps(data)))
+        json_data = json.loads(self.extracted_data)
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["biography"])
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["full_name"])
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["profile_pic_url_hd"])
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["username"])
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["followed_by"]["count"])
+        print(json_data["entry_data"]["ProfilePage"][0]["user"]["follows"]["count"])
+        for node in json_data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"]:
+            print(node["date"])
+            print(node["likes"]["count"])
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+scrapper = Scrapper(username,password)
+
+"""
+
+
+"""
